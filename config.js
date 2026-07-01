466821eb24669fe6899eeb128ce89b6689508a36
tipentaengenharia <ti.pentaengenharia@gmail.com>
2026-07-01 16:13:52 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * config.js
+ * ÚNICO arquivo que você precisa editar depois de publicar o backend.
+ * Cole aqui a URL da implantação (deployment) do Google Apps Script,
+ * terminada em "/exec".
+ *
+ * Exemplo:
+ * const API_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
+ */
+const API_URL = "https://script.google.com/macros/s/AKfycbwLut4JYAHczR1yoGyuczZMd64su0zvkTO126K0ZmHG3rgjJA6LJUNjK25BLbywfHt_/exec";
+
+// Nome usado como fallback antes de carregar as configurações do servidor
+const NOME_SISTEMA_PADRAO = "Ponto Fácil";
